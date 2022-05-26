9c3968b1dd541d61df4531fd56d79e5a4d038aa4
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-05-26 22:53:01 +0300

    Task 2 updated

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("Print the first number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Print the second number: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Print the third number: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > a) max = b;
+if (c > a) max = c;
+
+Console.Write("Max number is ");
+Console.Write(max);
