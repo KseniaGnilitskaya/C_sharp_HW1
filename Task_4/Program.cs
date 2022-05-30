cc2745ef5e5e0d40cbc04cd8650e6dda17346282
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-05-30 19:04:33 +0300

    Task 4 added

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,3 +1,8 @@
-﻿Console.Write("Print the number: ");
+﻿Console.Write("Type the number: ");
 int number = int.Parse(Console.ReadLine());
 
+int m =1;
+for (int i=m; i<=number; i++)
+if (i % 2 == 0){
+    Console.Write(i);
+}
